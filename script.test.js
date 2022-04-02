324e47eebf634f5db7c8ee2fcde1f159e84398d6
Simone <startae14@gmail.com>
2022-04-03 00:10:12 +0200

    Add tests starting from null and undefined input and with other cases

--- script.test.js
+++ script.test.js
@@ -8,11 +8,23 @@ dbMock = [
   'dogpictures.com'
 ]
 
-it('is a silly test', () => {
-  expect('hello').toBe('hello');
+describe('google search tests', () => {
+  it('is a silly test', () => {
+    expect('hello').toBe('hello');
+  });
+
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com']);
+  });
+
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com']);
-});
