40740ef2a27dd77eba14e4c8bdd55756a049cecb
Simone <startae14@gmail.com>
2022-04-02 23:08:25 +0200

    Added the script to test

---
+++ script.js
@@ -0,0 +1,17 @@
+const googleDatabase = [
+  'cats.com',
+  'souprecipes.com',
+  'flowers.com',
+  'animals.com',
+  'catpictures.com',
+  'myfavouritecats.com'
+];
+// basic google search algorhitm
+const googleSearch = searchInput => {
+  const matches = googleDatabase.filter(website => {
+    return website.includes(searchInput)
+  });
+  // return just the first 3 results
+  return matches.length > 3 ? matches.slice(0,3) : matches;
+}
+console.log(googleSearch('soup'));
