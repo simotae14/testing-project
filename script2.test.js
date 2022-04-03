4cca7e9de06593df4b056f79fec388b9f054c83c
Simone <startae14@gmail.com>
2022-04-03 23:41:13 +0200

    test async functions using expect.assertions and the done method

--- script2.test.js
+++ script2.test.js
@@ -6,9 +6,11 @@ const {
 } = require('./script2');
 
 // use the async/await function
-it('calls swapi to get people', () => {
+it('calls swapi to get people', (done) => {
+  expect.assertions(1);
   getPeople(fetch).then(({ count }) => {
     expect(count).toEqual(82);
+    done();
   });
 });
 
