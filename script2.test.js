407c986ee4e92c1696a737b032532789323743e1
Simone <startae14@gmail.com>
2022-04-03 23:34:44 +0200

    test async code

--- script2.test.js
+++ script2.test.js
@@ -4,3 +4,17 @@ const {
   getPeople,
   getPeoplePromise,
 } = require('./script2');
+
+// use the async/await function
+it('calls swapi to get people', () => {
+  getPeople(fetch).then(({ count }) => {
+    expect(count).toEqual(82);
+  });
+});
+
+// use the Promise
+it('calls swapi to get people with a promise', () => {
+  getPeoplePromise(fetch).then(({ count }) => {
+    expect(count).toEqual(82);
+  });
+});
