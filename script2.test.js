5c8775a0d272ce39d10197000b1baa0da64a9393
Simone <startae14@gmail.com>
2022-04-04 17:21:24 +0200

    mocked API with Jest

--- script2.test.js
+++ script2.test.js
@@ -21,3 +21,21 @@ it('calls swapi to get people with a promise', () => {
     expect(results.length).toBeGreaterThan(5);
   });
 });
+
+it('getPeople returns count and results', () => {
+  const mockFetch = jest.fn()
+    .mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+        count: 89,
+        results: [0,1,2,3,4,5],
+      })
+    }));
+  expect.assertions(5);
+  return getPeoplePromise(mockFetch).then(({ count, results }) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
+    expect(count).toEqual(89);
+    expect(results.length).toBeGreaterThan(5);
+    expect(mockFetch).toMatchSnapshot();
+  });
+});
